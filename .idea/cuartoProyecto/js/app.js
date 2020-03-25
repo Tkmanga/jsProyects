4922f6369b1f62880757384694dd27c2b1486fb1
Jose Dario Tacacho <tacachojose@gmail.com>
2020-03-25 12:07:21 -0300

    comenzando el 4to ejercicio

---
+++ .idea/cuartoProyecto/js/app.js
@@ -0,0 +1,20 @@
+
+/*    function Seguro() {
+
+    }
+    //esto nos trae el maximo año actual
+    const max = new Date().getFullYear(),
+        min = max -20;
+
+    const selectAnios = document.getElementById('anio');
+
+    for(let i = min; i<max; i++){
+        let option = document.createElement('option');
+        option.value = i; //le asignamos el valor
+        option.innerHTML = i; //le asignamos que muestre en el form
+        selectAnios.appendChild(option);
+    }
+
+
+ */
+
