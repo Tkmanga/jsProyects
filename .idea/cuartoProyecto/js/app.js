06568c621ae8e5495e03cc2045e5a73d6b3371f0
Jose Dario Tacacho <tacachojose@gmail.com>
2020-03-26 20:59:20 -0300

    comenzando el 4to ejercicio

--- .idea/cuartoProyecto/js/app.js
+++ .idea/cuartoProyecto/js/app.js
@@ -1,14 +1,21 @@
-
-/*    function Seguro() {
-
+//constructor para seguro
+ function Seguro(marca,anio,tipo) {
+    this.marca = marca;
+    this.anio = anio;
+    this.tipo = tipo;
     }
+//esto esta encargado del UI
+function Interfaz() {
+        
+    }
+    
     //esto nos trae el maximo año actual
     const max = new Date().getFullYear(),
         min = max -20;
 
     const selectAnios = document.getElementById('anio');
 
-    for(let i = min; i<max; i++){
+    for(let i = max; i>= min; i--){
         let option = document.createElement('option');
         option.value = i; //le asignamos el valor
         option.innerHTML = i; //le asignamos que muestre en el form
@@ -16,5 +23,5 @@
     }
 
 
- */
 
+console.log('asd');
