5f14ab8751b0684f4ef05e570fb676781dc83e45
Tkmanga <tacachojose@gmail.com>
2020-05-23 21:53:31 -0300

    Games

---
+++ .idea/Games/01/app.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded',() => {
+    const cardArray = [
+        {
+            name: 'maizena',
+            img: 'img/'
+        },
+        {
+            name: 'maizena',
+            img: ''
+        },
+        {
+            name: 'maizena',
+            img: ''
+        },
+        {
+            name: 'maizena',
+            img: ''
+        },
+        {
+            name: 'maizena',
+            img: ''
+        },
+        {
+            name: 'maizena',
+            img: ''
+        }
+    ];
+});
